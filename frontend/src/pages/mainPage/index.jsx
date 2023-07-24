b7efe7ae420a7d3f7bb4b096a2a89eb487453929
sdh20282 <sdh20282@gmail.com>
2023-07-24 13:04:37 +0900

    Feat : Styled-component 테마, Dropbox component 구현

--- frontend/src/pages/mainPage/index.jsx
+++ frontend/src/pages/mainPage/index.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components"
+
+import DropBox1 from "components/dropboxes/dropbox1/DropBox1"
+import { useEffect, useState } from "react"
+
+const Wrapper = styled.div`
+    background-color: ${props => props.theme.colors.theme.light_green};
+`
+
+const testList = [1, 2, 3, 4, 5];
+
 export default function MainPage() {
+    const [test, setTest] = useState("");
+
+    useEffect(() => {
+        setTest(testList[0]);
+    }, [])
+
     return (
-        <div>index</div>
+        <Wrapper>
+            <DropBox1 props={{ list: testList, target: test, callback: setTest }} />
+        </Wrapper>
     )
 }
