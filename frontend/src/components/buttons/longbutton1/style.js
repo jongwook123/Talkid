fa29098fb06cbaa73b089c2985f75cc6b3a33fc6
sdh20282 <sdh20282@gmail.com>
2023-07-24 13:39:54 +0900

    Feat : LongButton1 컴포넌트 높이, border radius 조절

--- frontend/src/components/buttons/longbutton1/style.js
+++ frontend/src/components/buttons/longbutton1/style.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 export const Button = styled.button`
     width: 100%;
+    height: 32px;
     border: 0;
     transition: all 0.2s;
     background-color: ${props => props.color === 'orange' ? `${props.theme.colors.theme.orange}` : props.color === 'green' ? `${props.theme.colors.theme.green}` : `${props.theme.colors.theme.blue}`};
+    border-radius: ${props => props.theme.border_radius.lv2};
     color: ${props => props.theme.colors.font.white};
     font-size: ${props => props.theme.font_size.lv3};
 
