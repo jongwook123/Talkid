80cd489feea11b56654678fdb457e3faffef4935
김선영 <rzlzlq@naver.com>
2023-07-26 14:54:10 +0900

    Feat : 비밀번호 수정 페이지 디자인 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MainPage from "pages/mainPage"
 import SigninPage from "pages/userPages/signinPage"
 import SignupPage from "pages/userPages/signupPage"
 import UserEditPage from "pages/userPages/userEditPage"
+import FindPasswordPage from "pages/userPages/findpasswordPage"
 
 export default function App() {
     return (
@@ -19,6 +20,7 @@ export default function App() {
                     <Route path="/signin" element={<SigninPage />} />
                     <Route path="/signup" element={<SignupPage />} />
                     <Route path="/modifyuser" element={<UserEditPage />} />
+                    <Route path="/findpassword" element={<FindPasswordPage />} />
                     
                 </Routes>
             </BrowserRouter>
