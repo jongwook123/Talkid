7651e909195168662f311501a5fb77a310987833
유영서 <you2882@naver.com>
2023-07-26 17:28:23 +0900

    Fix : 코드리뷰 후 수정

--- backend/src/main/java/com/talkids/backend/dto/FindPwDto.java
+++ backend/src/main/java/com/talkids/backend/dto/FindPwDto.java
@@ -1,5 +1,6 @@
 package com.talkids.backend.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -7,6 +8,8 @@ public class FindPwDto {
 
     @Data
     public static class Request {
+
+        @Email
         @NotEmpty(message = "이메일을 입력해주세요.")
         private String memberMail;
     }
