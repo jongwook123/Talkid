d45098c8d7b83d995f9c99c5ca5ed8c039962d2f
박종욱 <pju7429@naver.com>
2023-08-02 15:47:29 +0900

    Feat : 학생메인페이지 Threejs 구현

---
+++ frontend/src/pages/mainPage/Ground.jsx
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+
+export default function Ground(props) {
+  const groundGeo = new THREE.PlaneGeometry(300, 300); // 땅 지오메트리 생성
+  const groundMat = new THREE.MeshStandardMaterial({ color: "#c68a12" }); // 갈색 머티리얼 생성
+
+  return (
+    <mesh {...props} geometry={groundGeo} material={groundMat} receiveShadow rotation={[Math.PI / -2, 0, Math.PI]} />
+  );
+}
