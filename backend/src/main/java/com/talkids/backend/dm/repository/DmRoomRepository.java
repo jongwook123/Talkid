527d540e2ddb3cc4385d742c919c765e2f533f3e
유영서 <you2882@naver.com>
2023-08-04 02:14:50 +0900

    Fix : 최근 메세지 조회 수정

--- backend/src/main/java/com/talkids/backend/dm/repository/DmRoomRepository.java
+++ backend/src/main/java/com/talkids/backend/dm/repository/DmRoomRepository.java
@@ -13,13 +13,13 @@ public interface DmRoomRepository extends JpaRepository<DmRoom, String> {
     List<DmRoom> findAll();
     Optional<DmRoom> findByDmRoomId(String dmRoomId);
 
-    @Query("SELECT r.dmRoomId, COUNT(um), MAX(m.messageContent) " +
+    @Query("SELECT r.dmRoomId, COUNT(DISTINCT um), (SELECT m.messageContent FROM Message m WHERE r = m.dmRoom ORDER BY m.createdAt DESC LIMIT 1)" +
             "FROM DmRoom r " +
             "LEFT JOIN Message m ON r = m.dmRoom " +
             "LEFT JOIN UncheckMessage um ON r = um.dmRoom AND um.member.memberId = :memberId " +
             "WHERE r.dmRoomId IN (SELECT DISTINCT jr.dmRoom.dmRoomId FROM DmJoinMember jr WHERE jr.member.memberId = :memberId) " +
             "GROUP BY r.dmRoomId " +
-            "ORDER BY MAX(m.createdAt) DESC NULLS LAST")
+            "ORDER BY MAX(m.createdAt) DESC")
     List<Object[]> getChatRoomsAndUncheckCounts(@Param("memberId") int memberId);
 
     int deleteByDmRoomId(String dmRoomId);
