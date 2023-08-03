6bb03f3fd336387037d7a8e5f1d14978005168f9
이우철 <wc421@naver.com>
2023-08-03 14:36:55 +0900

    Feat : insert sql파일 추가

---
+++ backend/src/main/resources/member_type.sql
@@ -0,0 +1,3 @@
+INSERT INTO `member_type` (`member_type_id`, `member_type_name`) VALUES
+(1, '선생님'),
+(2, '학생');
---
+++ backend/src/main/resources/time_zone.sql
@@ -0,0 +1,5 @@
+INSERT INTO `time_zone` (`hour`, `minute`, `time_zone_name`) VALUES
+(9, 0, "Korea"),
+(8, 0, "China"),
+(-4, 0, "United States"),
+(9, 0, "Japan");
