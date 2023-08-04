04646f07d98969e3317a3793c8b3100ebda4692a
이우철 <wc421@naver.com>
2023-08-04 15:55:35 +0900

    Feat : Entity들의 DTO 생성 및 공통 로직
    - Entity마다 DTO 생성
    - LoginUser 어노테이션 추가

---
+++ backend/src/main/java/com/talkids/backend/member/dto/LanguageDto.java
@@ -0,0 +1,22 @@
+package com.talkids.backend.member.dto;
+
+import com.talkids.backend.member.entity.Language;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter @Builder
+public class LanguageDto {
+    private int languageId;
+    private String languageCode;
+    private String languageEng;
+    private String languageOri;
+
+    public static LanguageDto fromEntity(Language language){
+        return LanguageDto.builder()
+            .languageId(language.getLanguageId())
+            .languageCode(language.getLanguageCode())
+            .languageEng(language.getLanguageEng())
+            .languageOri(language.getLanguageOri())
+            .build();
+    }
+}
