2c463056c7b999d31007e5ea16daed43d5711280
이우철 <wc421@naver.com>
2023-08-08 22:16:14 +0900

    Fix : school_id 추가

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -408,11 +408,11 @@ INSERT INTO `s09p12d106`.`country` (`country_id`, `country_name`, `country_code`
 (252, 'Zimbabwe', 'ZWE');
 
 
-INSERT INTO `s09p12d106`.`school` (`country_id`, `school_lat`, `school_lng`, `time_zone_id`, `school_address`, `school_name`) VALUES
-(119, 36.027484, 129.343395, 1, "경상북도 포항시 남구 포스코대로 147-10", '이동초등학교'),
-(46, 39.91436584712231, 116.49522427478453, 2, "Beijing, Chaoyang, Balizhuang Rd, 八里庄西里78 邮政编码: 100123", '八里庄西里'),
-(239, 38.88321859907826, -76.98998347524216, 3, "420 12th St SE, Washington, DC 20003", 'Catharine Watkins Elementary School'),
-(112, 35.686626, 139.725081, 4, "160-0004 Tokyo, Shinjuku City, Yotsuya, 2 Chome−６", '新宿区立四谷子ども園');
+INSERT INTO `s09p12d106`.`school` (`school_id`, `country_id`, `school_lat`, `school_lng`, `time_zone_id`, `school_address`, `school_name`) VALUES
+(1, 119, 36.027484, 129.343395, 1, "경상북도 포항시 남구 포스코대로 147-10", '이동초등학교'),
+(2, 46, 39.91436584712231, 116.49522427478453, 2, "Beijing, Chaoyang, Balizhuang Rd, 八里庄西里78 邮政编码: 100123", '八里庄西里'),
+(3, 239, 38.88321859907826, -76.98998347524216, 3, "420 12th St SE, Washington, DC 20003", 'Catharine Watkins Elementary School'),
+(4, 112, 35.686626, 139.725081, 4, "160-0004 Tokyo, Shinjuku City, Yotsuya, 2 Chome−６", '新宿区立四谷子ども園');
 
 
 insert into `s09p12d106`.`bad_words`(words) values
