c7292a59c8068f7ed7290b154c9e41669177bde9
유영서 <you2882@naver.com>
2023-08-08 16:30:12 +0900

    Fix : Dto 이름 수정

--- backend/src/main/java/com/talkids/backend/group/dto/CreateGroupJoinMemberDto.java
+++ backend/src/main/java/com/talkids/backend/group/dto/CreateGroupJoinMemberDto.java
@@ -40,8 +40,8 @@ public class CreateGroupJoinMemberDto {
         private int totalExp;
         private int monthExp;
 
-        public static GroupJoinMemberDto.Response groupJoinMemberDto(Member member, int totalExp, int monthExp) {
-            GroupJoinMemberDto.Response response = new GroupJoinMemberDto.Response();
+        public static Response groupJoinMemberDto(Member member, int totalExp, int monthExp) {
+            Response response = new CreateGroupJoinMemberDto.Response();
             response.setMember(member);
             response.setTotalExp(totalExp);
             response.setMonthExp(monthExp);
