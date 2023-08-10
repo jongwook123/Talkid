48e1b27f4cac4724469b54a9437c0bc31ce338a3
유영서 <you2882@naver.com>
2023-08-10 15:31:23 +0900

    Feat : 팔로우/언팔로우 기능 구현

---
+++ backend/src/main/java/com/talkids/backend/member/entity/Follower.java
@@ -0,0 +1,41 @@
+package com.talkids.backend.member.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="Follwer")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Follower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="followerId")
+    private int followerId;
+
+    @Column(name="createdAt")
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    /* ---------------------------------- */
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="memberId")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="followerMember")
+    private Member followerMember;
+
+}
