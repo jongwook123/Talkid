e6c79573c24bef89dba13a18ca736efdd6a35b81
sdh20282 <sdh20282@gmail.com>
2023-08-18 01:57:36 +0900

    Fix : 알람 API 오타 수정

--- frontend/src/apis/NotifyAPIs.js
+++ frontend/src/apis/NotifyAPIs.js
@@ -3,7 +3,7 @@ import FetchTemplate from "utils/FetchTemplate";
 export const GetNotifys = async (token) => {
     try {
         const response = await FetchTemplate({
-            path: process.env.REACT_APP_BASE_SERVER + 'notify',
+            path: process.env.REACT_APP_BASE_SERVER + '/notify',
             headers: {
                 "Authorization" : `Bearer ${token}`
             },
